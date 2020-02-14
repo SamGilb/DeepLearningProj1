6e9eeed0410f3938ca8e67b0faafea632617fbd8
Jonathan Hillman <jonathandalehillman@gmail.com>
2020-02-13 17:04:41 -0700

    Add files via upload

--- GradientDescent/GradientDescent.R
+++ GradientDescent/GradientDescent.R
@@ -2,6 +2,9 @@ source("GradientDescentHelper.R")
 
 main <- function()
 {
+  #initialize
+  numIterVector = 1:(maxIterations + 1)
+  
   #############################################################################
   #############################################################################
   #                             First Data Set                                #
@@ -16,9 +19,9 @@ main <- function()
   classificationList <- createClassificationList(import_data)
   
   #make X and Y vector/matrix
+  y <- import_data[,58]
   X <- import_data[,1:57]
   X <- scale(X)
-  y <- import_data[,58]
   
   #separate data using classificationList as needed
   X_train <- X[classificationList == 0,]
@@ -30,29 +33,47 @@ main <- function()
   y_validation <- y[classificationList == 1]
   y_test <- y[classificationList == 2]
   
+  #print stuff for the report
+  print("0     1")
+  print(c("test",length(y_test[y_test==0]),length(y_test[y_test==1])))
+  print(c("train",length(y_train[y_train==0]),length(y_train[y_train==1])))
+  print(c("validation",length(y_validation[y_validation==0]),length(y_validation[y_validation==1])))
+  
   #store weightMatrix
   weightMatrix <- GradientDescent( X_train, y_train, stepSize, maxIterations )
   
   #initialize vector variables
   trainErrorVector = NA
   validationErrorVector = NA
-  trainLogLossVector = NA
+  testErrorVector = NA
+  trainMeanLogLossVector = NA
   validationMeanLogLossVector = NA 
+  #update vector variables
   for(i in 1:(maxIterations+1))
   {
     trainErrorVector[i] <- 1 - findAccuracy( X_train, y_train, weightMatrix[,i])
     validationErrorVector[i] <- 1 - findAccuracy( X_validation, y_validation, weightMatrix[,i] )
+    testErrorVector[i] <- 1 - findAccuracy( X_test, y_test, weightMatrix[,i])
     trainMeanLogLossVector[i] <- calculateMeanLogLoss( X_train, y_train, weightMatrix[,i] )
     validationMeanLogLossVector[i] <- calculateMeanLogLoss( X_validation, y_validation, weightMatrix[,i] )
   }
   
-  #plot the validation error with the training error on the same graph with respect
-  #to the number of iterations in the gradient descent
-  ggplot() +
-  geom_smooth(mapping=aes(1:ncol(weightMatrix),trainErrorVector),color = 'red') +
-  labs(x="Number of Iteartions") +
-  geom_smooth(mapping=aes(1:ncol(weightMatrix),validationErrorVector)) +
-  labs(x="Number of Iterations")
+  #print stuff for tables
+  trainMinError = 1 - findAccuracy(X_train, y_train, weightMatrix[,which.min(trainErrorVector)])
+  validationMinError = 1 - findAccuracy(X_validation, y_validation, weightMatrix[,which.min(validationErrorVector)])
+  testMinError = 1 - findAccuracy(X_test, y_test, weightMatrix[,which.min(testErrorVector)])
+  print("min log error for train, validation, test")
+  print(trainMinError)
+  print(validationMinError)
+  print(testMinError)
+  print("baseline for train, validation, test (subtract from 1 if needed)")
+  print(length(y_train[y_train==1])/length(y_train))
+  print(length(y_validation[y_validation==1])/length(y_validation))
+  print(length(y_test[y_test==1])/length(y_test))
+  
+  #plot how %error and mean log loss for data set
+  programPlot(numIterVector,trainErrorVector,validationErrorVector,trainMeanLogLossVector,validationMeanLogLossVector, weightMatrix)
+  
   #############################################################################
   #############################################################################
   #                              Second Data Set                              #
@@ -69,9 +90,10 @@ main <- function()
   classificationList <- createClassificationList(import_data)
   
   #make X and Y vector/matrix
-  X <- import_data[,1:ncol(import_data)-1]
+  y <- import_data[,ncol(import_data)]
+  X <- import_data[,2:10]
+  X <- clean(X)
   X <- scale(X)
-  y <- import_data[,ncol(import_data)-1]
   
   #separate data using classificationList as needed
   X_train <- X[classificationList == 0,]
@@ -83,27 +105,116 @@ main <- function()
   y_validation <- y[classificationList == 1]
   y_test <- y[classificationList == 2]
   
+  #print stuff for report
+  print("0     1")
+  print(c("test",length(y_test[y_test==0]),length(y_test[y_test==1])))
+  print(c("train",length(y_train[y_train==0]),length(y_train[y_train==1])))
+  print(c("validation",length(y_validation[y_validation==0]),length(y_validation[y_validation==1])))
+  
   #store weightMatrix
   weightMatrix <- GradientDescent( X_train, y_train, stepSize, maxIterations )
   
   #initialize vector variables
   trainErrorVector = NA
   validationErrorVector = NA
-  trainLogLossVector = NA
-  validationMeanLogLossVector = NA 
+  testErrorVector = NA
+  trainMeanLogLossVector = NA
+  validationMeanLogLossVector = NA
+  #update vector variables
   for(i in 1:(maxIterations+1))
   {
     trainErrorVector[i] <- 1 - findAccuracy( X_train, y_train, weightMatrix[,i])
     validationErrorVector[i] <- 1 - findAccuracy( X_validation, y_validation, weightMatrix[,i] )
+    testErrorVector[i] <- 1 - findAccuracy( X_test, y_test, weightMatrix[,i])
     trainMeanLogLossVector[i] <- calculateMeanLogLoss( X_train, y_train, weightMatrix[,i] )
     validationMeanLogLossVector[i] <- calculateMeanLogLoss( X_validation, y_validation, weightMatrix[,i] )
   }
+  
+  #print stuff for tables
+  trainMinError = 1 - findAccuracy(X_train, y_train, weightMatrix[,which.min(trainErrorVector)])
+  validationMinError = 1 - findAccuracy(X_validation, y_validation, weightMatrix[,which.min(validationErrorVector)])
+  testMinError = 1 - findAccuracy(X_test, y_test, weightMatrix[,which.min(testErrorVector)])
+  print("min log error for train, validation, test")
+  print(trainMinError)
+  print(validationMinError)
+  print(testMinError)
+  print("baseline for train, validation, test (subtract from 1 if needed)")
+  print(length(y_train[y_train==1])/length(y_train))
+  print(length(y_validation[y_validation==1])/length(y_validation))
+  print(length(y_test[y_test==1])/length(y_test))
+  
+  #plot how %error and mean log loss for data set
+  programPlot(numIterVector,trainErrorVector,validationErrorVector,trainMeanLogLossVector,validationMeanLogLossVector, weightMatrix)
   
   #############################################################################
   #############################################################################
   #                             Third Data Set                                #
   #############################################################################
   #############################################################################
+  #import data as a list
+  import_data <- fread('zip.train')
+  import_data <- matrix(as.numeric(unlist(import_data)), nrow(import_data), ncol(import_data))
+  import_data <- rbind(import_data[import_data[,1] == 0,], import_data[import_data[,1] == 1,])
+  
+  #randomized list of 0's 1's and 2's
+  classificationList <- createClassificationList(import_data)
+  
+  #make X and Y vector/matrix
+  y <- import_data[,1]
+  X <- import_data[,2:257]
+  X <- clean(X)
+  X <- scale(X)
+  
+  #separate data using classificationList as needed
+  X_train <- X[classificationList == 0,]
+  X_validation <- X[classificationList == 1,]
+  X_test <- X[classificationList == 2,]
+  
+  #separate y using 
+  y_train <- y[classificationList == 0]
+  y_validation <- y[classificationList == 1]
+  y_test <- y[classificationList == 2]
+  
+  #print stuff for report
+  print("0     1")
+  print(c("test",length(y_test[y_test==0]),length(y_test[y_test==1])))
+  print(c("train",length(y_train[y_train==0]),length(y_train[y_train==1])))
+  print(c("validation",length(y_validation[y_validation==0]),length(y_validation[y_validation==1])))
+  
+  #store weightMatrix
+  weightMatrix <- GradientDescent( X_train, y_train, stepSize, maxIterations )
+  
+  #initialize vector variables
+  trainErrorVector = NA
+  validationErrorVector = NA
+  testErrorVector = NA
+  trainMeanLogLossVector = NA
+  validationMeanLogLossVector = NA 
+  #update vector variables
+  for(i in 1:(maxIterations+1))
+  {
+    trainErrorVector[i] <- 1 - findAccuracy( X_train, y_train, weightMatrix[,i])
+    validationErrorVector[i] <- 1 - findAccuracy( X_validation, y_validation, weightMatrix[,i] )
+    testErrorVector[i] <- 1 - findAccuracy( X_test, y_test, weightMatrix[,i])
+    trainMeanLogLossVector[i] <- calculateMeanLogLoss( X_train, y_train, weightMatrix[,i] )
+    validationMeanLogLossVector[i] <- calculateMeanLogLoss( X_validation, y_validation, weightMatrix[,i] )
+  }
+  
+  #print stuff for pdf
+  trainMinError = 1 - findAccuracy(X_train, y_train, weightMatrix[,which.min(trainErrorVector)])
+  validationMinError = 1 - findAccuracy(X_validation, y_validation, weightMatrix[,which.min(validationErrorVector)])
+  testMinError = 1 - findAccuracy(X_test, y_test, weightMatrix[,which.min(testErrorVector)])
+  print("min log error for train, validation, test")
+  print(trainMinError)
+  print(validationMinError)
+  print(testMinError)
+  print("baseline for train, validation, test (subtract from 1 if needed)")
+  print(length(y_train[y_train==1])/length(y_train))
+  print(length(y_validation[y_validation==1])/length(y_validation))
+  print(length(y_test[y_test==1])/length(y_test))
+  
+  #plot how %error and mean log loss for data set
+  programPlot(numIterVector,trainErrorVector,validationErrorVector,trainMeanLogLossVector,validationMeanLogLossVector, weightMatrix)
   
   #Return a weightMatrix
   weightMatrix
@@ -111,62 +222,4 @@ main <- function()
 
 #run this big boy
 weightMatrix <- main()
-###############################################################################
-###############################################################################
-#                                          Testing                            #
-###############################################################################
-###############################################################################
-source("GradientDescentHelper.R")
-###############################################################################
-#                                       Stuff                                 #
-###############################################################################
-#import data as a list
-import_data <- fread('https://web.stanford.edu/~hastie/ElemStatLearn/datasets/spam.data')
-#convert list to matrix
-import_data <- matrix( unlist(import_data), nrow(import_data), ncol(import_data) )
-print("done downloading")
 
-#randomized list of 0's 1's and 2's
-classificationList <- createClassificationList(import_data)
-
-#make X and Y vector/matrix
-X <- import_data[,1:57]
-X <- scale(X)
-y <- import_data[,58]
-
-#separate data using classificationList as needed
-X_train <- X[classificationList == 0,]
-X_validation <- X[classificationList == 1,]
-X_test <- X[classificationList == 2,]
-
-#separate y using 
-y_train <- y[classificationList == 0]
-y_validation <- y[classificationList == 1]
-y_test <- y[classificationList == 2]
-
-weightMatrix <- GradientDescent( X_train, y_train, stepSize, maxIterations )
-###############################################################################
-#                                    End Stuff                                #
-###############################################################################
-trainErrorVector = NA
-validationErrorVector = NA
-trainMeanLogLossVector = NA
-validationMeanLogLossVector = NA
-for(i in 1:(maxIterations+1))
-{
-  trainErrorVector[i] <- 1 - findAccuracy(X_train,y_train,weightMatrix[,i])
-  validationErrorVector[i] <- 1 - findAccuracy( X_validation, y_validation, weightMatrix[,i] )
-  trainMeanLogLossVector[i] <- calculateMeanLogLoss(X_train, y_train, weightMatrix[,i])
-  validationMeanLogLossVector[i] <- calculateMeanLogLoss(X_validation,y_validation,weightMatrix[,i])
-}
-plot(1:501, trainMeanLogLossVector)
-plot(1:501, validationMeanLogLossVector)
-
-ggplot() +
-  geom_smooth(mapping=aes(1:ncol(weightMatrix),trainErrorVector),color = 'red') +
-  labs(X="Number of Iterations") +
-  geom_smooth(mapping=aes(1:ncol(weightMatrix),validationErrorVector), color = 'black') +
-  labs(X="Number of Iterations")
-
-errorMatrix <- cbind(trainErrorVector,validationErrorVector)
-ggplot(data = )
